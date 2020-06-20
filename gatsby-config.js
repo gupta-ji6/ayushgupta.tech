c9ddf96a55657588cc0bf3a04e56aa734165bc84
gupta-ji6 <ayushgupta197@gmail.com>
2020-06-20 21:10:36 +0530

    Updated PWA manifest, thanks to Dhruv

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,8 +16,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'Ayush Gupta',
+        short_name: 'GuptaJi',
         start_url: '/',
         background_color: config.darkNavyColor,
         theme_color: config.navyColor,
