cc80ab9b5e3132c8301308eae6e262525dd0de8d
gupta-ji6 <ayushgupta197@gmail.com>
2020-09-03 17:17:43 +0530

    :bento: Added apple app store icon in icons

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,19 +1,20 @@
-import IconLoader from './loader';
-import IconLogo from './logo';
-import IconGithub from './github';
-import IconLinkedin from './linkedin';
-import IconMedium from './medium';
-import IconInstagram from './instagram';
-import IconTwitter from './twitter';
-import IconFacebook from './facebook';
-import IconExternal from './external';
-import IconGooglePlay from './googleplay';
-import IconFolder from './folder';
-import IconArticle from './article';
-import IconLocation from './location';
-import IconStar from './star';
-import IconFork from './fork';
 import FormattedIcon from './formattedIcon';
+import IconAppStore from './appStore';
+import IconArticle from './article';
+import IconExternal from './external';
+import IconFacebook from './facebook';
+import IconFolder from './folder';
+import IconFork from './fork';
+import IconGithub from './github';
+import IconGooglePlay from './googleplay';
+import IconInstagram from './instagram';
+import IconLinkedin from './linkedin';
+import IconLoader from './loader';
+import IconLocation from './location';
+import IconLogo from './logo';
+import IconMedium from './medium';
+import IconStar from './star';
+import IconTwitter from './twitter';
 
 export {
   IconLoader,
@@ -26,6 +27,7 @@ export {
   IconTwitter,
   IconExternal,
   IconGooglePlay,
+  IconAppStore,
   IconFolder,
   IconArticle,
   IconLocation,
