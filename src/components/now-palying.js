de7a5da817fbbf13621532c4b23ac75f90e1113d
gupta-ji6 <ayushgupta197@gmail.com>
2020-12-22 17:54:17 +0530

    :sparkles: Add context over NowPlaying widget

--- src/components/now-palying.js
+++ src/components/now-palying.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { Fragment, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import { theme } from '@styles';
 import sr from '@utils/sr';
 import { srConfig } from '@config';
 import { IconSpotify } from '@components/icons';
+
+// ========================= CONSTANTS ===========================
 
 const { colors } = theme;
 
@@ -15,7 +17,85 @@ const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 const NOW_PLAYING_URL = 'https://api.spotify.com/v1/me/player/currently-playing';
 const SPOTIFY_PROFILE = 'https://open.spotify.com/user/31yuvamoxkbmkpvhpunh6xwoshii';
 
-const TrackContainer = styled.div`
+const NowPlayingContext = {
+  playing: [
+    {
+      emoji: '💫',
+      copy: 'vibing to',
+    },
+    {
+      emoji: '🎵',
+      copy: 'currently listening to',
+    },
+    {
+      emoji: '😇',
+      copy: 'tripping on',
+    },
+    {
+      emoji: '🥰',
+      copy: 'mushing over',
+    },
+    {
+      emoji: '🙈',
+      copy: 'gushing over',
+    },
+    {
+      emoji: '🗣',
+      copy: 'lip syncing to',
+    },
+    {
+      emoji: '👻',
+      copy: 'quietly murmuring',
+    },
+  ],
+  notPlaying: [
+    {
+      emoji: '🤷🏻‍♀️',
+      copy: `maybe I'm bored of my playlist`,
+    },
+    {
+      emoji: '💤',
+      copy: `there's a high chance I'm sleeping`,
+    },
+    {
+      emoji: '🎧',
+      copy: 'probably my headphones died',
+    },
+    {
+      emoji: '📺',
+      copy: 'chilling on Netflix, maybe?',
+    },
+    {
+      emoji: '🥂',
+      copy: `maybe today's the day to socialize`,
+    },
+    {
+      emoji: '📸',
+      copy: `probably I'm out with my camera`,
+    },
+    {
+      emoji: '🤝🏻',
+      copy: `maybe I had to stop music to attend a meeting`,
+    },
+  ],
+};
+
+// ========================= UTILITY FUNCTIONS ===========================
+
+const randomArrayIndex = arr => Math.floor(Math.random() * arr.length);
+
+// ========================= STYLED COMPONENTS ===========================
+
+const TrackContext = styled.div`
+  font-size: smaller;
+  margin-bottom: 10px;
+
+  span {
+    font-size: small;
+  }
+`;
+
+const NowPlayingWidget = styled.div`
   border: 1px solid ${colors.green};
   border-radius: 3px;
   padding: 9px;
@@ -23,6 +103,11 @@ const TrackContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  transition: all 0.3s ease-in-out;
+
+  &:hover {
+    transform: scale3d(0.95, 0.95, 0.95);
+  }
 `;
 
 const AlbumImage = styled.img`
@@ -70,10 +155,14 @@ const SpotifyIcon = styled.div`
   justify-content: flex-end;
 `;
 
+// ========================= COMPONENT ===========================
+
 const NowPlaying = () => {
   const [track, setTrack] = useState({});
   const revealContainer = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
 
+  // get OAuth access token from Spotify Web API
   const getAccessToken = async () => {
     const urlencoded = new URLSearchParams();
     urlencoded.append('grant_type', 'refresh_token');
@@ -91,6 +180,7 @@ const NowPlaying = () => {
     return response.json();
   };
 
+  // fetch the current playing track, if any
   const fetchCurrentTrack = async () => {
     const { access_token } = await getAccessToken();
 
@@ -105,6 +195,7 @@ const NowPlaying = () => {
       if (response.status === 200) {
         const jsonResponse = await response.json();
         setTrack(jsonResponse);
+        setIsPlaying(true);
       }
     } catch (err) {
       console.error(err);
@@ -117,30 +208,62 @@ const NowPlaying = () => {
     fetchCurrentTrack();
   }, []);
 
+  // function which returns a custom copy to show above now playing widget
+  const fetchNowPlayingCopy = () => {
+    if (isPlaying) {
+      const index = randomArrayIndex(NowPlayingContext.playing);
+      return (
+        <Fragment>
+          <div>
+            <span role="img" aria-label="music-emoji">
+              {NowPlayingContext.playing[index].emoji}
+            </span>
+          </div>
+          <div>{NowPlayingContext.playing[index].copy}</div>
+        </Fragment>
+      );
+    } else {
+      const index = randomArrayIndex(NowPlayingContext.notPlaying);
+      return (
+        <Fragment>
+          <div>
+            <span role="img" aria-label="music-emoji">
+              {NowPlayingContext.notPlaying[index]?.emoji}
+            </span>
+          </div>
+          <div>{NowPlayingContext.notPlaying[index]?.copy}</div>
+        </Fragment>
+      );
+    }
+  };
+
   return (
-    <TrackContainer>
-      <a
-        href={track?.item?.external_urls?.spotify || SPOTIFY_PROFILE}
-        target="_blank"
-        rel="nofollow noopener noreferrer">
-        <AlbumImage
-          src={track?.item?.album?.images[0].url || 'https://source.unsplash.com/128x128/?music'}
-          loading="lazy"
-        />
-      </a>
-      <a
-        href={track?.item?.external_urls?.spotify || SPOTIFY_PROFILE}
-        target="_blank"
-        rel="nofollow noopener noreferrer">
-        <TrackInfo>
-          <TrackName>{track?.item?.name || 'Not Playing'}</TrackName>
-          <AlbumName>{track?.item?.album?.name || 'Spotify'}</AlbumName>
-        </TrackInfo>
-      </a>
-      <SpotifyIcon>
-        <IconSpotify />
-      </SpotifyIcon>
-    </TrackContainer>
+    <div>
+      <TrackContext>{fetchNowPlayingCopy()}</TrackContext>
+      <NowPlayingWidget>
+        <a
+          href={track?.item?.external_urls?.spotify || SPOTIFY_PROFILE}
+          target="_blank"
+          rel="nofollow noopener noreferrer">
+          <AlbumImage
+            src={track?.item?.album?.images[0].url || 'https://source.unsplash.com/128x128/?music'}
+            loading="lazy"
+          />
+        </a>
+        <a
+          href={track?.item?.external_urls?.spotify || SPOTIFY_PROFILE}
+          target="_blank"
+          rel="nofollow noopener noreferrer">
+          <TrackInfo>
+            <TrackName>{track?.item?.name || 'Not Playing'}</TrackName>
+            <AlbumName>{track?.item?.album?.name || 'Spotify'}</AlbumName>
+          </TrackInfo>
+        </a>
+        <SpotifyIcon>
+          <IconSpotify />
+        </SpotifyIcon>
+      </NowPlayingWidget>
+    </div>
   );
 };
 
