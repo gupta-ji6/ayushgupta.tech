e95a0268f4f73241166a6ede2085de7a5314c35c
gupta-ji6 <ayushgupta197@gmail.com>
2021-01-19 16:31:52 +0530

    :truck: Move content folder to root directory

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,21 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'content',
-        path: `${__dirname}/src/content/`,
+        path: `${__dirname}/content/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/content/projects`,
       },
     },
     {
