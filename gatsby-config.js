40494ce9d4958f5951621fe61e03b439e4dce203
gupta-ji6 <ayushgupta197@gmail.com>
2021-02-03 11:36:58 +0530

    :chart_with_upwards_trend: Respect DNT

--- gatsby-config.js
+++ gatsby-config.js
@@ -180,7 +180,7 @@ module.exports = {
           // Puts tracking script in the head instead of the body
           head: false,
           // Setting this parameter is also optional
-          respectDNT: false,
+          respectDNT: true,
         },
       },
     },
