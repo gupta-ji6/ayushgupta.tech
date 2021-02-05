59f0dd5b8482341173143a1cb53b82db20a0fee4
gupta-ji6 <ayushgupta197@gmail.com>
2021-02-05 20:37:03 +0530

    :sparkles: Add toast when PWA is ready to update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,15 +6,47 @@
 
 // You can delete this file if you're not using it
 
-// import React from 'react';
+const React = require('react');
 const splitbee = require('@splitbee/web');
+const { toast } = require('react-hot-toast');
+const theme = require('./src/styles/theme');
+
+// console.log(theme);
+const { colors } = theme.default;
+
+const updateButtonStyles = {
+  margin: '0px',
+  marginLeft: '10px',
+  backgroundColor: `${colors.green}`,
+  color: `${colors.lightestNavy}`,
+  padding: '6px',
+  borderRadius: '0.3em',
+  border: 'none',
+  fontWeight: 'bold',
+};
 
 // Called when the Gatsby browser runtime first starts
 exports.onClientEntry = () => {
-  //   console.log('client has entered the chat!');
+  // console.log('client has entered the chat!');
   splitbee.default.init({
     disableCookie: true, // will disable the cookie usage
     scriptUrl: '/bee',
     apiUrl: '/_hive',
   });
+};
+
+// Reference - https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/#onServiceWorkerUpdateReady
+// Also see - https://github.com/gatsbyjs/gatsby/issues/9087#issuecomment-723294431
+
+// Inform plugins when a service worker has been updated in the background and the page is ready to reload to apply changes.
+exports.onServiceWorkerUpdateReady = () => {
+  // eslint-disable-next-line no-unused-vars
+  toast(t => (
+    <span>
+      A new version is available!
+      <button onClick={() => window.location.reload()} style={updateButtonStyles}>
+        Update
+      </button>
+    </span>
+  ));
 };
