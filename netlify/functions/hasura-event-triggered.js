400a02ebdc204689b2237be31b2cc16b26b70624
gupta-ji6 <ayushgupta197@gmail.com>
2021-02-19 12:53:44 +0530

    :alembic: Update netlify function & remove bad-words

--- netlify/functions/hasura-event-triggered.js
+++ netlify/functions/hasura-event-triggered.js
@@ -1,37 +1,14 @@
-// with thanks to https://github.com/vnovick/netlify-function-example/blob/master/functions/bad-words.js
-const axios = require('axios');
-const Filter = require('bad-words');
-const filter = new Filter();
-const hgeEndpoint = 'https://ayushgupta-tech-blog.herokuapp.com/v1/graphql';
+// ref - https://github.com/hasura/graphql-engine/blob/master/community/boilerplates/event-triggers/netlify-functions/nodejs/echo/functions/index.js
 
-const query = `
-mutation verifiedp($id: uuid!, $title: String!, $content: String!) {
-  update_posts(_set: { verified: true, content: $content, title: $title }, 
-    where:{ id: { _eq: $id } }) {
-    returning {
-      id
-    }
-  }
-}
-`;
+exports.handler = (event, context, callback) => {
+  const {
+    event: { op, data },
+    table: { name, schema },
+  } = JSON.parse(event.body);
 
-exports.handler = async event => {
-  let request;
-  try {
-    request = JSON.parse(event.body);
-  } catch (e) {
-    return { statusCode: 400, body: 'cannot parse hasura event' };
-  }
-
-  const variables = {
-    id: request.event.data.new.id,
-    title: filter.clean(request.event.data.new.title),
-    content: filter.clean(request.event.data.new.content),
-  };
-  try {
-    await axios.post(hgeEndpoint, { query, variables });
-    return { statusCode: 200, body: 'success' };
-  } catch (err) {
-    return { statusCode: 500, body: err.toString() };
-  }
+  // Send the response
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({ msg: 'Received event!', data: { op, data, name, schema } }),
+  });
 };
