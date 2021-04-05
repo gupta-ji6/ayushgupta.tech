e5a57bf4f01c31b0f7b6f71617b94e9e6ddc6863
gupta-ji6 <ayushgupta197@gmail.com>
2021-04-05 13:14:41 +0530

    :bento: Replace Medium logo with new

--- src/components/icons/medium.js
+++ src/components/icons/medium.js
@@ -1,17 +1,35 @@
 import React from 'react';
 
 const IconMedium = () => (
-  <svg
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 50 50"
-    width="24px"
-    height="24px"
-    strokeWidth="1.5"
-    stroke="currentColor"
-    strokeLinecap="round"
-    strokeLinejoin="round">
-    <path d="M30,43h17l-4-4l-0.027-28.496l3.891-4.49H34.845l-9.24,22.73L15.497,6H3l4,5.091v24.195L1,43h14l-6-7.714V13.75L22,43 l-0.002,0.014L34,13.545V39L30,43z M10.911,41H5.089L8,37.258L10.911,41z M21.922,37.899L10.828,12.938L8.633,8h5.564l9.581,21.556 l0.805,1.81L21.922,37.899z M35.414,40.414L36,39.828V39V13.545v-5.06l0.191-0.47h6.293l-1.022,1.18l-0.489,0.565l0.001,0.747 L41,39.002l0.001,0.827l0.585,0.585L42.172,41h-7.343L35.414,40.414z" />
+  <svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" viewBox="0 0 256 256">
+    <path fill="none" d="M0 0H256V256H0z"></path>
+    <ellipse
+      cx="72"
+      cy="128"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="20"
+      rx="56"
+      ry="60"></ellipse>
+    <ellipse
+      cx="184"
+      cy="128"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="20"
+      rx="24"
+      ry="56"></ellipse>
+    <path
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="20"
+      d="M240 72L240 184"></path>
   </svg>
 );
 
