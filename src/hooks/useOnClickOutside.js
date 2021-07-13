13865fa523a805f7a842fd44a2f9d0703be47912
gupta-ji6 <ayushgupta197@gmail.com>
2021-07-14 02:01:11 +0530

    :arrow_right_hook: Add useOnClickOutside & useScrollDirection hook

---
+++ src/hooks/useOnClickOutside.js
@@ -0,0 +1,39 @@
+import { useEffect } from 'react';
+
+/**
+ * This hook allows you to detect clicks outside of a specified element.
+ * @see {@link https://usehooks.com/useOnClickOutside/}
+ * @param {*} ref
+ * @param {*} handler
+ */
+const useOnClickOutside = (ref, handler) => {
+  useEffect(
+    () => {
+      const listener = event => {
+        // Do nothing if clicking ref's element or descendent elements
+        if (!ref.current || ref.current.contains(event.target)) {
+          return;
+        }
+
+        handler(event);
+      };
+
+      document.addEventListener('mousedown', listener);
+      document.addEventListener('touchstart', listener);
+
+      return () => {
+        document.removeEventListener('mousedown', listener);
+        document.removeEventListener('touchstart', listener);
+      };
+    },
+    // Add ref and handler to effect dependencies
+    // It's worth noting that because passed in handler is a new ...
+    // ... function on every render that will cause this effect ...
+    // ... callback/cleanup to run every render. It's not a big deal ...
+    // ... but to optimize you can wrap handler in useCallback before ...
+    // ... passing it into this hook.
+    [ref, handler],
+  );
+};
+
+export default useOnClickOutside;
